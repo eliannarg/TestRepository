d64a426a833a53e94cc31c8b52b82f57757e0c8a
eliannarg <60122226+eliannarg@users.noreply.github.com>
2026-01-21 20:32:45 -0400

    Add files via upload

---
+++ untitled.m
@@ -0,0 +1,7 @@
+sdrinfo
+openExample('comm/FMModulationAndDemodulationExample')
+
+mex -setup
+openExample('comm/QPSKTransmitterAndReceiverSimulinkExample')
+openExample('comm/QPSKTransmitterAndReceiverSimulinkExample')
+imshow(imagen_data); 
